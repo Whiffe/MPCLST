5483d93c3d507ee69a23b7a5c8b726a89144c182
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-06-13 15:22:30 +0800

    Update addDatasetXX.sh

--- Dataset/addDatasetXX.sh
+++ Dataset/addDatasetXX.sh
@@ -10,5 +10,6 @@ mkdir -p ${newDataset}/frames
 mkdir -p ${newDataset}/choose_frames_all
 mkdir -p ${newDataset}/choose_frames
 mkdir -p ${newDataset}/annotations
+mkdir -p ${newDataset}/choose_frames_middle
 
 touch ${newDataset}/cutVideos.txt
