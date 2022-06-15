c9c82c32406e83a6feac2f7b6fd0d96657986ebf
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-06-15 17:05:08 +0800

    Update json_extract.py

--- Dataset/json_extract.py
+++ Dataset/json_extract.py
@@ -26,12 +26,6 @@ for root, dirs, files in os.walk(choose_frames_middle_dir, topdown=False):
                 line = f.readline()
                 viaJson = json.loads(line)
                 
-                
-                attributeNum1 = len(viaJson['attribute']['1']['options'])
-                attributeNum2 = len(viaJson['attribute']['2']['options'])+attributeNum1
-                #attributeNum3 = len(viaJson['attribute']['3']['options'])+attributeNum2
-                attributeNums = [0,attributeNum1,attributeNum2]
-                
                 files = {}
                 for file in viaJson['file']:
                     fid = viaJson['file'][file]['fid']
@@ -48,22 +42,23 @@ for root, dirs, files in os.walk(choose_frames_middle_dir, topdown=False):
                     videoName = fname.split('_')[0]
                     #获取视频帧ID
                     frameId = int((int(fname.split('_')[1].split('.')[0])-1)/30)
-                    for action in imagen_x['av']:
-                        avs = imagen_x['av'][action]
+                    
+                    # 获取坐标对应的图片，因为最后的坐标值需要在0到1
+                    # 就需要用现有坐标值/图片大小
+                    imgPath = root + '/' + videoName + "_" + str(frameId*30+1).zfill(6) + '.jpg'
+                    imgTemp = cv2.imread(imgPath)  #读取图片信息
+                    print(imgPath)
+                    sp = imgTemp.shape #[高|宽|像素值由三种原色构成]
+                    img_H = sp[0]
+                    img_W = sp[1]
+                    
+                    for action in imagen_x['av'][0]:
+                        avs = imagen_x['av'][0][action]
                         #行为复选框不为空,获取复选框中的行为
                         if avs != '':
                             #一个复选框可能有多个选择
                             avArr = avs.split(',')
                             for av in avArr:
-                                
-                                # 获取坐标对应的图片，因为最后的坐标值需要在0到1
-                                # 就需要用现有坐标值/图片大小
-                                imgPath = root + '/' + videoName + "_" + str(frameId*30+1).zfill(6) + '.jpg'
-                                imgTemp = cv2.imread(imgPath)  #读取图片信息
-                                print(imgPath)
-                                sp = imgTemp.shape #[高|宽|像素值由三种原色构成]
-                                img_H = sp[0]
-                                img_W = sp[1]
                                 x1 = xy[0] / img_W
                                 y1 = xy[1] / img_H
                                 x2 = (xy[0]+xy[2]) / img_W
@@ -90,7 +85,7 @@ for root, dirs, files in os.walk(choose_frames_middle_dir, topdown=False):
                                 if y2 > 1:
                                     y2 = 1
                                 
-                                actionId = attributeNums[int(action)-1]+int(av)+1
+                                actionId = int(av)+1
                                 dict = [videoName,frameId,x1,y1,x2,y2,actionId]
                                 
                                 
