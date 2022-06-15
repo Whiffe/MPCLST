86851eb9d05d82e38b94a8a3914ff707c8d65a72
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-06-15 17:38:55 +0800

    Update dense_proposals_train_deepsort.py

--- yolovDeepsort/mywork/dense_proposals_train_deepsort.py
+++ yolovDeepsort/mywork/dense_proposals_train_deepsort.py
@@ -2,18 +2,26 @@ import sys
 import os
 import json
 import pickle
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--yoloLabel_dir', default='../yolov5/runs/detect/newExp',type=str, help="Label path for yolov5")
+parser.add_argument('--avaMin_dense_proposals_dir', default='./avaMin_dense_proposals_train.pkl',type=str, help="avaMin_dense_proposals_dir")
+parser.add_argument('--show', default='show',type=str, help="show")
+
+arg = parser.parse_args()
 
 #传参 labelPath是yolov5检测结果的位置，需要获取0（0代表人）的四个坐标值，还需要检测概率
-# ../yolov5/runs/detect/exp/labels
-labelPath = sys.argv[1]
+labelPath = arg.yoloLabel_dir
 
 #传参 保存为pkl的地址，这是像ava数据集对齐
 # ./avaMin_dense_proposals_train.pkl
-avaMin_dense_proposals_path = sys.argv[2]
+avaMin_dense_proposals_path = arg.avaMin_dense_proposals_dir
 
 #传参 是否可视化
 #可见就传入 show，否则不填
-showPkl = sys.argv[3]
+showPkl = arg.show
 
 results_dict = {}
 dicts = []
@@ -43,11 +51,11 @@ for root, dirs, files in os.walk(labelPath):
             
                     # 由于yolov5的检测结果是 xywh
                     # 要将xywh转化成xyxy
-                    y = j
+                    y = j.copy()
                     y[1] = float(j[1]) - float(j[3]) / 2  # top left x
                     y[2] = float(j[2]) - float(j[4]) / 2  # top left y
-                    y[3] = float(j[1]) + float(j[3])  # bottom right x
-                    y[4] = float(j[2]) + float(j[4])  # bottom right y
+                    y[3] = float(j[1]) + float(j[3]) / 2  # bottom right x
+                    y[4] = float(j[2]) + float(j[4]) / 2  # bottom right y
                     
                     results.append([y[1],y[2],y[3],y[4],y[5]])
                     temp_txt.close()  # 关闭文件
